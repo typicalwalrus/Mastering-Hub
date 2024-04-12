ae316323b3a90010fe9752770fa9b62929e9ebfd
typicalwalrus <156853488+typicalwalrus@users.noreply.github.com>
2024-04-12 14:55:53 -0400

    Added files 240412

---
+++ script.js
@@ -0,0 +1,33 @@
+const audioAlpha = new (AudioContext || webkitURL.AudioContext())();
+
+// creating buffer node
+const bufferNode = audioAlpha.createBufferSource();
+bufferNode.connect(audioAlpha.destination);
+
+// Just a placeholder, will work out a way to update the sample rate based on the sample rate of the file.
+let sampleRate = 44100;
+
+// this is going to be an array value of the loudness values of the file once I work out the hosting system.
+let loudnessArray = [];
+
+/**
+ * function to calculate intergrated LUFS
+ * @param {Array} loudnessArray
+ * @param {Number} sampleRate
+ * @returns {Number} intergrated LUFS Value
+ */
+const calcLUFS = function (loudnessArray, sampleRate) {
+  let sumSquared = 0;
+  for (let i = 0; i < loudnessArray.length; i++) {
+    sumSquared += loudnessArray[i] * loudnessArray[i];
+  }
+
+  const rms = Math.sqrt(sumSquared / loudnessArray.length);
+  const loudnessDB = 20 * Math.log10(rms);
+
+  // Normalization
+  const intergratedLUFS = loudnessDB + 0;
+  return intergratedLUFS;
+};
+
+// I don't really fully understand this math
